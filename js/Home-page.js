42e90f3f192d0079a31b398783212571905d214b
ahmedSadoun <ahmedsaadoun866@gmail.com>
2024-04-18 15:33:51 +0200

    edited navigation viewer, and add cyclic url

--- js/Home-page.js
+++ js/Home-page.js
@@ -1,7 +1,5 @@
-import { print } from "./x.mjs";
-print();
 let columnNumber = 4;
-let dataUrl = "https://c5b3762fa879446cbabfc42b27b4ca6b.api.mockbin.io/";
+let dataUrl = " https://cerise-monkey-tutu.cyclic.app";
 let rootEntities = [];
 async function fetchEntities() {
   let res = await fetch(dataUrl);
@@ -24,7 +22,7 @@ function buildColumns(entities, entityIndex) {
                 folder-identifier=${element.entity_id}
               type="button"
               class="btn btn-outline-dark overflow-x-hidden"
-              style="width: 100%"
+              style="width: 100%;height:100%"
             >
               <div class="d-flex">
                 <div
@@ -32,23 +30,11 @@ function buildColumns(entities, entityIndex) {
                   role="img"
                   aria-label="Shared Google Drive Folder"
                 >
-                  <svg
-                    focusable="false"
-                    viewBox="0 0 24 24"
-                    height="24px"
-                    width="24px"
-                    fill="#5f6368"
-                    class="a-s-fa-Ha-pa"
-                  >
-                    <g>
-                      <path
-                        d="M20 6h-8l-2-2H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm-5 3c1.1 0 2 .9 2 2s-.9 2-2 2-2-.9-2-2 .9-2 2-2zm4 8h-8v-1c0-1.33 2.67-2 4-2s4 .67 4 2v1z"
-                      ></path>
-                      <path d="M0 0h24v24H0z" fill="none"></path>
-                    </g>
-                  </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 32 32">
+                <linearGradient id="KA3iPnJF2lqt7U2-W-Vona_oiCA327R8ADq_gr1" x1="16" x2="16" y1="4.905" y2="27.01" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#00b5f0"></stop><stop offset="1" stop-color="#008cc7"></stop></linearGradient><path fill="url(#KA3iPnJF2lqt7U2-W-Vona_oiCA327R8ADq_gr1)" d="M26,27H6c-1.105,0-2-0.895-2-2V7c0-1.105,0.895-2,2-2h4.027c0.623,0,1.22,0.247,1.66,0.688	l0.624,0.624C12.753,6.753,13.35,7,13.973,7H26c1.105,0,2,0.895,2,2v16C28,26.105,27.105,27,26,27z"></path><linearGradient id="KA3iPnJF2lqt7U2-W-Vonb_oiCA327R8ADq_gr2" x1="16" x2="16" y1="5" y2="27" gradientUnits="userSpaceOnUse"><stop offset="0" stop-opacity=".02"></stop><stop offset="1" stop-opacity=".15"></stop></linearGradient><path fill="url(#KA3iPnJF2lqt7U2-W-Vonb_oiCA327R8ADq_gr2)" d="M26,7H13.973	c-0.623,0-1.22-0.247-1.66-0.688l-0.625-0.625C11.247,5.247,10.65,5,10.027,5H6C4.895,5,4,5.895,4,7v18c0,1.105,0.895,2,2,2h20	c1.105,0,2-0.895,2-2V9C28,7.895,27.105,7,26,7z M27.75,25c0,0.965-0.785,1.75-1.75,1.75H6c-0.965,0-1.75-0.785-1.75-1.75V7	c0-0.965,0.785-1.75,1.75-1.75h4.027c0.56,0,1.087,0.218,1.484,0.615l0.625,0.625c0.491,0.491,1.143,0.761,1.837,0.761H26	c0.965,0,1.75,0.785,1.75,1.75V25z"></path><linearGradient id="KA3iPnJF2lqt7U2-W-Vonc_oiCA327R8ADq_gr3" x1="16" x2="16" y1="8.922" y2="27.008" gradientUnits="userSpaceOnUse"><stop offset="0" stop-color="#00dcff"></stop><stop offset=".859" stop-color="#00bfff"></stop><stop offset="1" stop-color="#00a8e0"></stop></linearGradient><path fill="url(#KA3iPnJF2lqt7U2-W-Vonc_oiCA327R8ADq_gr3)" d="M27,27H5c-1.105,0-2-0.895-2-2V11	c0-1.105,0.895-2,2-2h22c1.105,0,2,0.895,2,2v14C29,26.105,28.105,27,27,27z"></path><linearGradient id="KA3iPnJF2lqt7U2-W-Vond_oiCA327R8ADq_gr4" x1="16" x2="16" y1="9" y2="27" gradientUnits="userSpaceOnUse"><stop offset="0" stop-opacity=".02"></stop><stop offset="1" stop-opacity=".15"></stop></linearGradient><path fill="url(#KA3iPnJF2lqt7U2-W-Vond_oiCA327R8ADq_gr4)" d="M27,9H5c-1.105,0-2,0.895-2,2v14	c0,1.105,0.895,2,2,2h22c1.105,0,2-0.895,2-2V11C29,9.895,28.105,9,27,9z M28.75,25c0,0.965-0.785,1.75-1.75,1.75H5	c-0.965,0-1.75-0.785-1.75-1.75V11c0-0.965,0.785-1.75,1.75-1.75h22c0.965,0,1.75,0.785,1.75,1.75V25z"></path>
+                </svg>
                 </div>
-                <div class="d-flex">${element.entity_name}</div>
+                <div class=" align-content-center">${element.entity_name}</div>
               </div>
             </button>
           </div>
@@ -57,13 +43,13 @@ function buildColumns(entities, entityIndex) {
       entitiesColumnString =
         entitiesColumnString +
         `
-          <div class="col mt-2" style="max-width:25%">
+          <div class="col mt-2 mt-2" style="max-width:25%">
             <button
               onclick="onFileClick(this)"
                 file-identifier=${element.entity_id}
               type="button"
               class="btn btn-outline-dark overflow-x-hidden"
-              style="width: 100%"
+              style="width: 100%;height:100%"
             >
               <div class="d-flex">
                 <div
@@ -71,11 +57,11 @@ function buildColumns(entities, entityIndex) {
                   role="img"
                   aria-label="Shared Google Drive Folder"
                 >
-                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24px" height="24px" viewBox="0 0 50 50">
-                <path d="M 7 2 L 7 48 L 43 48 L 43 14.59375 L 42.71875 14.28125 L 30.71875 2.28125 L 30.40625 2 Z M 9 4 L 29 4 L 29 16 L 41 16 L 41 46 L 9 46 Z M 31 5.4375 L 39.5625 14 L 31 14 Z"></path>
-                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="100" height="100" viewBox="0 0 50 50">
+<path fill="#16d6fa" d="M43,47H7V3h25.669L43,13.427V47z M11,43h28V15.073L31.001,7H11V43z"></path><polygon fill="#16d6fa" points="43,19 27,19 27,3 31,3 31,15 43,15"></polygon><rect width="18" height="4" x="16" y="23" fill="#16d6fa"></rect><rect width="18" height="4" x="16" y="31" fill="#16d6fa"></rect><rect width="4" height="4" x="27" y="3" fill="#00a4ff"></rect><rect width="4" height="4" x="39" y="15" fill="#00a4ff"></rect>
+</svg>
                 </div>
-                <div class="d-flex">${element.entity_name}</div>
+                <div class="align-content-center">${element.entity_name}</div>
               </div>
             </button>
           </div>
@@ -103,7 +89,7 @@ function buildEntitiesGrid(entities) {
   for (let index = 0; index < rowsCount; index++) {
     entitiesBodyString =
       entitiesBodyString +
-      `<div class="row d-flex p-2" style="min-width: 500px">`;
+      `<div class="row d-flex p-2 mt-3" style="min-width: 500px; height:100px ">`;
     let columns = buildColumns(entities, entityIndex);
     entityIndex = columns.entityIndex + 1;
     entitiesBodyString = entitiesBodyString + columns.entitiesColumnString;
@@ -132,24 +118,17 @@ function onFolderClick(folder) {
   let entity_id = folder.getAttribute("folder-identifier");
   let currentEntity = filterEntities(rootEntities, entity_id);
 
-  addToNavigationList(currentEntity);
   addToCacheObj(currentEntity);
   window.location.href = `./Entity-Page.html?entity_id=${entity_id}`;
 }
 
-function addToNavigationList(currentEntity) {
-  let navigationItem = {};
-  navigationItem.entity_id = currentEntity.entity_id;
-  navigationItem.entity_name = currentEntity.entity_name;
-  navigationItem.entity_type = currentEntity.entity_type;
-  sessionStorage.setItem("navigationList", JSON.stringify([navigationItem]));
-}
 function addToCacheObj(currentEntity) {
   let cacheObj = {};
   cacheObj[`entity_${currentEntity.entity_id}_sub_entities`] =
     currentEntity.entity_folder_sub_folders;
   sessionStorage.setItem("currentEntities", JSON.stringify(cacheObj));
 }
+
 function onFileClick(file) {
   let entity_id = file.getAttribute("file-identifier");
   let currentEntity = filterEntities(rootEntities, entity_id);
@@ -157,6 +136,6 @@ function onFileClick(file) {
     "fileSource",
     JSON.stringify(currentEntity.entity_file_content)
   );
-  addToNavigationList(currentEntity);
+
   window.location.href = `../HTMl/Preview-file.html?entity_id=${entity_id}`;
 }
--- js/Preview-File-Page.js
+++ js/Preview-File-Page.js
@@ -1,8 +1,8 @@
-function previewFile() {
-  let fileSource = JSON.parse(sessionStorage.getItem("fileSource"));
-  document.getElementById(
-    "previewFile"
-  ).innerHTML = ` <embed id="filePreviewer" src=${fileSource}  width="800px" height="1000px" />`;
+let dataUrl = " https://cerise-monkey-tutu.cyclic.app/subEntities";
+async function fetchEntities(entity_id) {
+  let res = await fetch(`${dataUrl}/${entity_id}`);
+  res = await res.json();
+  return res;
 }
 function getEntityIdFromUrl() {
   let urlString = window.location.href;
@@ -14,70 +14,72 @@ function getEntityIdFromUrl() {
   }
   return params;
 }
-function navigationDrawer() {
-  let navigationEntities = JSON.parse(sessionStorage.getItem("navigationList"));
-
+function navigationDrawer(navigationEntities) {
   let navigationEntitiesBodyString = `<p>
-    <a
-      href="./Home-Page.html"
-      class="link-secondary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover"
-      >Home</a
-    >
-  </p>`;
+  <a
+    href="./Home-Page.html"
+    class="link-secondary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover"
+    >Home</a
+  >
+</p>`;
 
+  if (!navigationEntities || navigationEntities.length <= 0) {
+    document.getElementById("navigation").innerHTML =
+      navigationEntitiesBodyString;
+    return;
+  }
+  console.log("ssssssss", navigationEntities);
   for (let index = 0; index < navigationEntities.length; index++) {
     let element = navigationEntities[index];
+
     navigationEntitiesBodyString =
       navigationEntitiesBodyString +
       `<svg
-          class="a-s-fa-Ha-pa c-qd"
-          width="24px"
-          height="24px"
-          viewBox="0 0 24 24"
-          focusable="false"
-          fill="currentColor"
-        >
-          <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"></path>
-        </svg>`;
+        class="a-s-fa-Ha-pa c-qd"
+        width="24px"
+        height="24px"
+        viewBox="0 0 24 24"
+        focusable="false"
+        fill="currentColor"
+      >
+        <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z"></path>
+      </svg>`;
     // }
     if (element.entity_type === "FOLDER") {
       navigationEntitiesBodyString =
         navigationEntitiesBodyString +
         `
-        <p>
-          <a
-            href="./Entity-Page.html?entity_id=${element.entity_id}"
-            class="link-secondary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover"
-            >${element.entity_name}</a
-          >
-        </p>`;
+      <p>
+        <a
+          href="./Entity-Page.html?entity_id=${element.entity_id}"
+          class="link-secondary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover"
+          >${element.entity_name}</a
+        >
+      </p>`;
     } else {
       navigationEntitiesBodyString =
         navigationEntitiesBodyString +
         `
-        <p>
-          <a
-            href="./Preview-file.html?entity_id=${element.entity_id}"
-            class="link-secondary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover"
-            >${element.entity_name}</a
-          >
-        </p>`;
+      <p>
+        <a
+          href="./Preview-file.html?entity_id=${element.entity_id}"
+          class="link-secondary link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover"
+          >${element.entity_name}</a
+        >
+      </p>`;
     }
   }
 
   document.getElementById("navigation").innerHTML =
     navigationEntitiesBodyString;
 }
-function editNavigationList() {
-  let parent_entity_id = getEntityIdFromUrl().entity_id;
-  let navigationEntities = JSON.parse(sessionStorage.getItem("navigationList"));
-  for (var i = 0; i < navigationEntities.length; i++) {
-    if (navigationEntities[i].entity_id == parent_entity_id) {
-      navigationEntities.splice(i + 1, navigationEntities.length - 1); // At the current index, remove one element
-    }
-  }
-  sessionStorage.setItem("navigationList", JSON.stringify(navigationEntities));
-  navigationDrawer();
+
+async function previewFile() {
+  let entity_id = getEntityIdFromUrl().entity_id;
+  let fileSource = await fetchEntities(entity_id);
+  navigationDrawer(fileSource.entity_path);
+  document.getElementById(
+    "previewFile"
+  ).innerHTML = ` <embed id="filePreviewer" src=${fileSource.entity_file_content}  width="800px" height="1000px" />`;
 }
-editNavigationList();
 previewFile();
